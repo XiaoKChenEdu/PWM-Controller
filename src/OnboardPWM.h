816114efd725c285001c529ee86d5fcb87515087
XiaoKChenEdu <xiaokang@hawaii.edu>
2025-06-05 12:40:42 -1000

    Update OnboardPWM.h

--- src/OnboardPWM.h
+++ src/OnboardPWM.h
@@ -15,16 +15,11 @@ public:
 
     void begin() override {
         // Arduino PWM is ready by default
-        // Pin 3 uses Timer2, pin 9&10 use Timer1, pin 5&6 use Timer0
     }
 
     void setPWMFreq(float frequency) override {
         currentFrequency = frequency;
         
-        // For servo applications, we'll use Arduino's default PWM frequency
-        // Pin 3 (Timer2) default frequency is ~490Hz which works for servos
-        // Note: Custom frequency control on Arduino requires complex timer manipulation
-        // For servo control, the pulse width timing is more important than frequency
     }
 
     void setPWM(int pin, int dutyCycle) override {
